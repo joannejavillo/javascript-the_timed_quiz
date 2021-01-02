a4b4b170d97ba9e43503ef9525aa4c12f1a901da
Joan J <joanne.javillo@gmail.com>
2021-01-02 16:36:49 +1030

    added questions

---
+++ assets/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "The most popular CSS Framework for developing responsive and mobile-first websites?",
+        choices: ["Bootshoes", "Bootstrap", "Beltstrap", "Bootcamp"],
+        answer: "Bootstrap",
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Home Text Markup Language", "Hyper Text Markup Language"],
+        answer: "Hyper Text Markup Language",
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Creative Style Sheets", "Colourful Style Sheets", "Cascading Style Sheets", "Computer Sttyle Sheets"],
+        answer: "Cascading Style Sheets",
+    },
+    {
+        question: "Where is the correct place to insert Javascript?",
+        choices: ["The <body> section", "The <head> section", "Both the <head> and <body> section", "The <footer> section"],
+        answer: "The <body> section",
+    },
+    {
+        question: "How can you add a comment in a Javascript?",
+        choices: ["//This is a comment", "||This is a comment", "<!--This is a comment-->", "&&This is a comment&&"],
+        answer: "//This is a comment"
+    }
+
+];

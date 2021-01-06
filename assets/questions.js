6a619134298c52db17ee45545b8fe53ea67d6c0b
Joan J <joanne.javillo@gmail.com>
2021-01-06 23:16:59 +1030

    modified questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,28 +1,55 @@
 var questions = [
     {
-        question: "The most popular CSS Framework for developing responsive and mobile-first websites?",
+        title: "The most popular CSS Framework for developing responsive and mobile-first websites?",
         choices: ["Bootshoes", "Bootstrap", "Beltstrap", "Bootcamp"],
         answer: "Bootstrap",
     },
     {
-        question: "What does HTML stand for?",
+       title: "What does HTML stand for?",
         choices: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Home Text Markup Language", "Hyper Text Markup Language"],
         answer: "Hyper Text Markup Language",
     },
     {
-        question: "What does CSS stand for?",
+        title: "What does CSS stand for?",
         choices: ["Creative Style Sheets", "Colourful Style Sheets", "Cascading Style Sheets", "Computer Sttyle Sheets"],
         answer: "Cascading Style Sheets",
     },
     {
-        question: "Where is the correct place to insert Javascript?",
+        title: "Where is the correct place to insert Javascript?",
         choices: ["The <body> section", "The <head> section", "Both the <head> and <body> section", "The <footer> section"],
         answer: "The <body> section",
     },
     {
-        question: "How can you add a comment in a Javascript?",
+        title: "How can you add a comment in a Javascript?",
         choices: ["//This is a comment", "||This is a comment", "<!--This is a comment-->", "&&This is a comment&&"],
         answer: "//This is a comment"
     }
 
 ];
+
+
+//function to start start quiz 
+function start(){
+    i = 0
+    score = 0
+    num1 = 60;
+    populate(i);
+    home.style.display = 'none';
+    end.style.display = 'none';
+    hscore.style.display = 'none';
+    ask.style.display = 'block';
+    feedback.innerHTML = "";
+    
+  }
+  
+  
+  //finish div hide question and display end
+  function finish(){
+    ask.style.display = 'none';
+    end.style.display = 'block';
+    playerInput.focus();
+    save.disabled = true;
+    mark.textContent = score;
+  
+        
+  }
